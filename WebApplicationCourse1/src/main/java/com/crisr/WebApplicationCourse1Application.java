57a50b63b62d677fd6e1a9b750f5404fa45627b9
crisr <crisr@localhost>
2021-05-13 16:30:02 -0600

    Converted from jar to war

--- WebApplicationCourse1/src/main/java/com/crisr/WebApplicationCourse1Application.java
+++ WebApplicationCourse1/src/main/java/com/crisr/WebApplicationCourse1Application.java
@@ -2,13 +2,20 @@ package com.crisr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.crisr.security.AppProperties;
 
 @SpringBootApplication
-public class WebApplicationCourse1Application {
+public class WebApplicationCourse1Application extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplicationCourse1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplicationCourse1Application.class, args);
